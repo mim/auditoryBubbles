cd522deb4ec324a28c3732583686e3546ce59ef7
Michael Mandel <mim@mr-pc.org>
2012-12-07 10:47:54 -0500

    made bubble noise directory generator aware of existing files and not overwrite them, added snr to dir name

--- mixMrtBubbleNoiseDir.m
+++ mixMrtBubbleNoiseDir.m
@@ -8,17 +8,26 @@ if ~exist('nReps', 'var') || isempty(nReps), nReps = 1; end
 if ~exist('dur_s', 'var') || isempty(dur_s), dur_s = 2; end
 
 useHoles = true;
-snr = db2mag(-30);
+snr_db = -30;
+snr = db2mag(snr_db);
 
 for i = 1:length(speechFiles)
     cleanFile = fullfile(inDir, speechFiles{i});
     [~,sr] = wavread(cleanFile);
     
+    num = 0;
     for r = 1:nReps
         [d f e] = fileparts(speechFiles{i});
-        outFile = fullfile(outDir, sprintf('bps%g', bubblesPerSec), d, ...
-            sprintf('%s%02d%s', f, r, e));
-        fprintf('%d %d: %s\n', i, r, outFile)
+
+        % Find next available file name
+        numTaken = true;
+        while numTaken
+            num = num + 1;
+            outFile = fullfile(outDir, sprintf('bps%g', bubblesPerSec), ...
+                sprintf('snr%+d', snr_db), d, sprintf('%s%02d%s', f, num, e));
+            numTaken = exist(outFile, 'file');
+        end
+        fprintf('%d %d: %s\n', i, num, outFile)
         
         [mix sr] = mixBubbleNoise(cleanFile, sr, useHoles, bubblesPerSec, snr, dur_s);
         
