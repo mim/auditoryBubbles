41f926e8baa15b75431c5a6e3607034c5e043c13
Michael Mandel <mim@mr-pc.org>
2013-04-25 15:08:48 -0400

    made it possible to have more than 6 choices and pick 6 of them randomly in generating csv for mturk

--- genCsvForMturk.m
+++ genCsvForMturk.m
@@ -1,4 +1,4 @@
-function urlLines = genCsvForMturk(words, s3dir, mixes, fileTag, nRep, urlsPerHit)
+function urlLines = genCsvForMturk(words, s3dir, mixes, fileTag, nRep, urlsPerHit, wordsPerUrl)
 
 % Generate a CSV file for use in mechanical turk
 %
@@ -13,18 +13,19 @@ function urlLines = genCsvForMturk(words, s3dir, mixes, fileTag, nRep, urlsPerHi
 %
 % Final url for each word is http://s3.amazonaws.com/mim.cse.osu/auditoryBubbles/[s3dir]/[word][mix].wav
 
+if ~exist('wordsPerUrl', 'var') || isempty(wordsPerUrl), wordsPerUrl = min(6, length(words)); end
 if ~exist('urlsPerHit', 'var') || isempty(urlsPerHit), urlsPerHit = 5; end
 if ~exist('nRep', 'var') || isempty(nRep), nRep = 3; end
 if ~exist('fileTag', 'var'), fileTag = ''; end
 
-baseUrl = 'http://s3.amazonaws.com/mim.cse.osu/auditoryBubbles';
+baseUrl = 'https://s3.amazonaws.com/mim.cse.osu/auditoryBubbles';
 outFile = fullfile('Z:\data\mrt\mturk_csv_in', [fileTag '_' datestr(clock, 30) '.csv']);
 
 % Create header
 header = {};
 for u = 1:urlsPerHit
     header{end+1} = sprintf('url%d', u);
-    for w = 1:length(words)
+    for w = 1:wordsPerUrl
         header{end+1} = sprintf('word%d%d', u, w);
     end
 end
@@ -36,6 +37,11 @@ for w = 1:length(words)
         for r = 1:nRep
             url = sprintf('%s/%s/%s%s.wav', baseUrl, s3dir, words{w}, mixes{m});
             ord = randperm(length(words));
+            ord = ord(1:wordsPerUrl);
+            if ~any(w == ord)
+                truePos = ceil(rand(1)*wordsPerUrl);
+                ord(truePos) = w;
+            end
             urlLines{end+1} = [{url} words(ord)];
         end
     end
@@ -43,9 +49,10 @@ end
 urlLines = urlLines(randperm(length(urlLines)));
 
 % Combine per-url choices into HITs of urlsPerHit
-for i = 1:(length(urlLines)-1)/urlsPerHit
+for i = 1:length(urlLines)/urlsPerHit
     ind = (i-1)*urlsPerHit+1 : i*urlsPerHit;
     lines{i} = cat(2, urlLines{ind});
 end
+fprintf('Created %d HITs\n', length(lines))
 
 csvWriteCells(outFile, [{header} lines]);
