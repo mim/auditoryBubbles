5a4afab87b053dd07e99cbc56d04d270a1cbb89e
Michael Mandel <mim@mr-pc.org>
2013-05-10 16:49:32 -0400

    added new classifiers, new features, PCA, repetitions

--- svmImportance.m
+++ svmImportance.m
@@ -1,4 +1,4 @@
-function [map features] = svmImportance(word, path, groupedFile, nFold, nTrain, nAvg, thresh)
+function mcr = svmImportance(word, path, groupedFile, pcaDim, nRep, thresh, nFold, nTrain, nAvg)
 
 % Train an SVM to predict intelligibility of mixtures from time-frequency SNR
 %
@@ -15,25 +15,32 @@ function [map features] = svmImportance(word, path, groupedFile, nFold, nTrain, 
 % training data.  thresh is the minimum fraction correct to count as
 % "intelligible".
 
-if ~exist('thresh', 'var') || isempty(thresh), thresh = 0.5; end
+if ~exist('thresh', 'var') || isempty(thresh), thresh = [0.3 0.7]; end
 if ~exist('nFold', 'var') || isempty(nFold), nFold = 5; end
 if ~exist('nTrain', 'var') || isempty(nTrain), nTrain = inf; end
 if ~exist('nAvg', 'var') || isempty(nAvg), nAvg = inf; end
+if ~exist('pcaDim', 'var') || isempty(pcaDim), pcaDim = 50; end
+if ~exist('nRep', 'var') || isempty(nRep), nRep = 1; end
+
+if numel(thresh) == 1, thresh = [thresh thresh]; end
+thresh = sort(thresh);
 
 % Should contain a variable "grouped"
 load(groupedFile);
 ansFile = grouped(:,3);
 
-mixPaths = {}; fracRight = [];
+mixPaths = {}; fracRight = []; isRight = [];
 for i = 1:length(ansFile)
     if ~reMatch(ansFile{i}, [word '\d\d\.wav']), continue, end
     mixPath = fullfile(path, ansFile{i});
-    if exist(mixPath, 'file')
+    cls = (grouped{i,5} >= thresh(2)) - (grouped{i,5} <= thresh(1));
+    if exist(mixPath, 'file') && cls
         mixPaths{end+1} = mixPath;
         fracRight(end+1) = grouped{i,5};
+        isRight(end+1) = cls;
     end
 end
-mixPaths = mixPaths(:); fracRight = fracRight(:);
+mixPaths = mixPaths(:); fracRight = fracRight(:); isRight = isRight(:);
 fprintf('Found %d mixes\n', length(mixPaths));
 
 cleanFile = fullfile(path, [word '.wav']);
@@ -45,30 +52,67 @@ for i = 1:length(mixPaths)
     [features(i,:) origShape] = computeFeatures(clean, mix);
 end
 
-isRight = fracRight >= thresh;
-fprintf('Average label value: %g %g\n', mean(fracRight), mean(isRight))
+fprintf('Average label value: %g %g\n', mean(fracRight), mean(isRight > 0))
 
-mcr = lassoXVal(features, fracRight, origShape)
-%mcr = linear_train(double(isRight), sparse(features), '-s 2 -v 50 -q');
-%mcr = crossval('mcr', features, isRight, 'Predfun', @(Xtr, ytr, Xte) libLinearPredFun(Xtr, ytr, Xte, origShape), 'leaveout', 1)
-%mcr = crossval('mcr', features, isRight, 'Predfun', @(Xtr, ytr, Xte) svmPredFun(Xtr, ytr, Xte, origShape), 'leaveout', 1)
+plotMeanStuff(features, isRight > 0, origShape);
 
-svm = svmtrain(features, isRight);
-map = reshape(svm.Alpha' * svm.SupportVectors, origShape);
+[pcs,pcaFeat] = princomp(zscore(features), 'econ');
+%pcaFeat = pcaFeat(:,1:pcaDim);
+%pcs = pcs(:,1:pcaDim);
 
+%xvalArgs = {'leaveout', 1};
+xvalArgs = {'kfold', 5};
+
+for p = 1:length(pcaDim)
+    fprintf('.')
+
+    try
+        svm = svmtrain(pcaFeat(:,1:pcaDim(p)), isRight);
+        subplots(reshape(-(svm.Alpha' * svm.SupportVectors) * pcs(:,1:pcaDim(p))', origShape)), drawnow
+    catch
+        fprintf('Oops\n')
+    end
+
+    for rep = 1:nRep
+        try
+            %mcr(p, rep) = lassoXVal(features, fracRight, origShape)
+            %mcr(p, rep) = linear_train(double(isRight), sparse(features), '-s 2 -v 41 -q');
+            %mcr(p, rep) = crossval('mcr', features, isRight, 'Predfun', @(Xtr, ytr, Xte) libLinearPredFun(Xtr, ytr, Xte, origShape), xvalArgs{:})
+            %mcr(p, rep) = crossval('mcr', features, isRight, 'Predfun', @(Xtr, ytr, Xte) svmPredFun(Xtr, ytr, Xte, origShape), xvalArgs{:})
+            mcr(p, rep) = crossval('mcr', pcaFeat(:,1:pcaDim(p)), isRight, 'Predfun', @(Xtr, ytr, Xte) svmPredFun(Xtr, ytr, Xte, [1 pcaDim(p)]), xvalArgs{:});
+            %mcr(p, rep) = crossval('mcr', features, isRight>0, 'Predfun', @ldaPredFun, xvalArgs{:})
+        catch
+            mcr(p, rep) = NaN;
+        end
+    end
+end
+fprintf('\n')
 
 function preds = svmPredFun(Xtr, ytr, Xte, shape)
+options = statset('MaxIter', 1e5);
+svm   = svmtrain(Xtr, ytr, 'options', options);
+preds = svmclassify(svm, Xte);
+%subplots(reshape(svm.Alpha' * svm.SupportVectors, shape)), drawnow
+
+function preds = svmPcaPredFun(Xtr, ytr, Xte, pcaDims, shape)
+[~,rec] = pcares(zscore([Xtr; Xte]), pcaDims);
+Xtr = rec(1:size(Xtr,1),:);
+Xte = rec(size(Xtr,1)+1:end,:);
 svm   = svmtrain(Xtr, ytr);
 preds = svmclassify(svm, Xte);
-subplots(reshape(svm.Alpha' * svm.SupportVectors, shape)), drawnow
+%subplots(reshape(svm.Alpha' * svm.SupportVectors, shape)), drawnow
 
 function preds = libLinearPredFun(Xtr, ytr, Xte, shape)
 svm = linear_train(double(ytr), sparse(Xtr), '-s 2 -q -c 1');
 cls = linear_predict(zeros(size(Xte,1),1), sparse(Xte), svm, '-q');
-preds = cls > 1;
+n = nominal(getlevels(ytr), getlabels(ytr));
+preds = n(cls);
 %sign = (2-svm.Label(1))*2-1;
 sign = 3 - 2*svm.Label(1);
-%subplots(sign * reshape(svm.w, shape)), drawnow
+subplots(sign * reshape(svm.w, shape)), drawnow
+
+function preds = ldaPredFun(Xtr, ytr, Xte)
+preds = classify(Xte, Xtr, ytr, 'diaglinear');
 
 function mcr = lassoXVal(X, y, shape)
 [b fitinfo] = lasso(X, y, 'CV', 3, 'NumLambda', 9, 'Alpha', 0.1);
@@ -82,10 +126,14 @@ function [feat origShape] = computeFeatures(clean, mix)
 % spectrogram.
 noise = mix - clean;
 snr = db(clean) - db(noise);
+
 %origFeat = snr;
 %origFeat = lim(snr, -30, 30);
-origFeat = db(noise);
+origFeat = -db(noise);
 %origFeat = [db(noise), -snr]; 
+%origFeat = max(-100, db(clean .* (db(noise) < -30))) + 0.1*randn(size(clean));
+
+origFeat = origFeat(:,30:end-29);
 origShape = size(origFeat);
 feat = reshape(origFeat, 1, []);
 
@@ -97,3 +145,16 @@ win_s = 0.064;
 win = round(win_s * fs);
 hop = round(win/4);
 spec = stft(x', win, win, hop);
+
+
+function plotMeanStuff(features, isRight, shape)
+
+feat1 = features( isRight,:);
+feat0 = features(~isRight,:);
+mn1 = mean(feat1);
+mn0 = mean(feat0);
+sd01 = std([bsxfun(@minus, feat1, mn1); bsxfun(@minus, feat0, mn0)]);
+dPrime = (mn1 - mn0) ./ sd01;
+
+subplots(listMap(@(x) reshape(x, shape), {mn0, mn1, dPrime}))
+drawnow
