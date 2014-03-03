5a2b59e7f79cb5863829f529017902009d6d7fe0
Michael Mandel <mim@mr-pc.org>
2014-03-03 15:25:58 -0500

    moved general packUp function to parent matlab directory, made wrapper here

---
+++ packUpBubbleNoise.m
@@ -0,0 +1,26 @@
+function files = packUpBubbleNoise()
+
+% Copy necessary files into a dist directory and test that everything works
+% with the path cleared.
+
+dest = 'D:\My Box Files\bubblesShannon\code\';
+
+files = {
+    'playListeningTestDir.m'
+    'csvWriteCells.m'
+    'playCalibrationFile.m'
+    'Z:\code\matlab\listMap.m'
+    'Z:\code\matlab\findFiles.m'
+    'Z:\code\matlab\reMatch.m'
+    'Z:\code\matlab\split.m'
+    'Z:\code\matlab\ensureDirExists.m'
+    'Z:\code\matlab\join.m'
+    'Z:\code\matlab\wavReadBetter.m'
+    };
+
+dataFiles = {
+    };
+
+fnsThatShouldRun = {@() playListeningTestDir('D:\mixes\shannon\sorted\forTesting', 'packUp')};
+
+packUp(dest, files, dataFiles, fnsThatShouldRun);
