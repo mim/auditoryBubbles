a3d751dab55643a7a66c38ca2347b6d42abc51b0
Michael Mandel <mim@mr-pc.org>
2014-03-16 16:15:08 -0400

    collect numbers of training instances as well

--- collectResInterspeech14.m
+++ collectResInterspeech14.m
@@ -14,15 +14,19 @@ xvalAcc(7,:,:) = mean(xvalAcc,1);
 printLatexTable(xvalAcc(:,:,1), '%0.1f', 'Cross validation per utterance');
 
 
-allCount = loadXvalNumTest(2:6:36);  % Same talker
-count(1:3,:,:) = allCount(1:3,:,:);
-allCount = loadXvalNumTest(1:6:36);  % Different talker
-count(4:6,:,:) = allCount([1 3 4],:,:);
-count(7,:,:) = sum(count,1);
+[allNTe allNTr] = loadXvalNumTest(2:6:36);  % Same talker
+nTe(1:3,:,:) = allNTe(1:3,:,:);
+nTr(1:3,:,:) = allNTr(1:3,:,:);
+[allNTe allNTr] = loadXvalNumTest(1:6:36);  % Different talker
+nTe(4:6,:,:) = allNTe([1 3 4],:,:);
+nTe(7,:,:) = sum(nTe,1);
+nTr(4:6,:,:) = allNTr([1 3 4],:,:);
+nTr(7,:,:) = mean(nTr,1);
 
 % rows: same talker v1,2,3, different talker 2,3,4, total
 % cols: acha, ada, afa, aja, ata, ava
-printLatexTable(count(:,:,1), '%d', 'Number of test instances per utterance');
+printLatexTable(nTr(:,:,1), '%0.1f', 'Number of training instances per utterance');
+printLatexTable(nTe(:,:,1), '%d', 'Number of test instances per utterance');
 
 
 % train 2, test 1, same talker
@@ -31,10 +35,12 @@ table2 = [permute(mean(t2t1SameAcc,1), [3 2 1]);
     permute(mean(t2t1SameAccDiffWord,1), [3 2 1])];
 printLatexTable(table2, '%0.1f', 'Cross validation accuracy for single talker');
 
-[t2t1SameCount t2t1SameCountDiffWord] = loadTtNumTest(2:6:36, 'trainSvmOnAllButOne');
-table2c = [permute(sum(t2t1SameCount, 1), [3 2 1]);
-    permute(sum(t2t1SameCountDiffWord, 1), [3 2 1])];
-printLatexTable(table2c, '%d', 'Number of test instances for single talker');
+[t2t1SameNTe t2t1SameNTeDiffWord t2t1SameNTr] = loadTtNumTest(2:6:36, 'trainSvmOnAllButOne');
+table2te = [permute(sum(t2t1SameNTe, 1), [3 2 1]);
+    permute(sum(t2t1SameNTeDiffWord, 1), [3 2 1])];
+table2tr = permute(mean(t2t1SameNTr, 1), [3 2 1]);
+printLatexTable(table2tr, '%d', 'Number of training instances for single talker');
+printLatexTable(table2te, '%d', 'Number of test instances for single talker');
 
 % train 2, test 1, different talker
 [t2t1DiffAcc t2t1DiffAccDiffWord] = loadAcc(1:6:36, 'trainSvmOnAllButOne');
@@ -42,10 +48,12 @@ table3 = [permute(mean(t2t1DiffAcc,1), [3 2 1]);
     permute(mean(t2t1DiffAccDiffWord,1), [3 2 1])];
 printLatexTable(table3, '%0.1f', 'Cross validation accuracy for different talkers');
 
-[t2t1DiffCount t2t1DiffCountDiffWord] = loadTtNumTest(1:6:36, 'trainSvmOnAllButOne');
-table3c = [permute(sum(t2t1DiffCount, 1), [3 2 1]);
-    permute(sum(t2t1DiffCountDiffWord, 1), [3 2 1])];
-printLatexTable(table3c, '%d', 'Number of test instances for different talkers');
+[t2t1DiffNTe t2t1DiffNTeDiffWord t2t1DiffNTr] = loadTtNumTest(1:6:36, 'trainSvmOnAllButOne');
+table3te = [permute(sum(t2t1DiffNTe, 1), [3 2 1]);
+    permute(sum(t2t1DiffNTeDiffWord, 1), [3 2 1])];
+table3tr = permute(mean(t2t1DiffNTr, 1), [3 2 1]);
+printLatexTable(table3tr, '%d', 'Number of training instances for different talkers');
+printLatexTable(table3te, '%d', 'Number of test instances for different talkers');
 
 
 function [allRes allResDiffWord] = loadAcc(targets, fn)
@@ -62,9 +70,8 @@ for target = 1:length(targets)
     end
 end
 
-function nTe = loadXvalNumTest(targets)
+function [nTe nTr] = loadXvalNumTest(targets)
 grouping = 0;
-nTe = [];
 fn = 'xvalSvmOnEachWord';
 for target = 1:length(targets)
     for doWarp = [0 1]
@@ -72,16 +79,18 @@ for target = 1:length(targets)
         res = load(resFile);
         for v = 1:length(res.data)
             nTe(v,target,doWarp+1) = sum(sum(cat(1, res.data{v}.nTe), 1), 2);
+            nTr(v,target,doWarp+1) = mean(sum(cat(1, res.data{v}.nTr), 2), 1);
         end
     end
 end
 
-function [nTe nTeDw] = loadTtNumTest(targets, fn)
+function [nTe nTeDw nTr] = loadTtNumTest(targets, fn)
 grouping = 0;
 for target = 1:length(targets)
     for doWarp = [0 1]
         resFile = resFileFor(grouping, doWarp, targets(target), fn);
         res = load(resFile);
+        nTr(:,target,doWarp+1) = sum(res.nTr, 2);
         rnTe = sum(res.nTe, 3);
         nTe(:,target,doWarp+1) = rnTe(:,1);
         nTeDw(:,target,doWarp+1) = rnTe(:,2);
