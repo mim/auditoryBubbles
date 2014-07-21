593ac02cdc157dfdc2e418175a83d1d9100006d0
Michael Mandel <mim@mr-pc.org>
2014-07-21 15:32:21 -0400

    Fixed bug in genMaskedSsn with window size being twice as big as requested
    This was also present in genBubbleNoise where this code was factored out
    of

--- genMaskedSsn.m
+++ genMaskedSsn.m
@@ -8,13 +8,13 @@ if ~exist('mask', 'var'), mask = []; end
 if ~exist('window_s', 'var') || isempty(window_s), window_s = 0.064; end
 if ~exist('hopFrac', 'var') || isempty(hopFrac), hopFrac = 0.25; end
 if ~exist('noiseShape', 'var') || isempty(noiseShape), noiseShape = 0; end
+fadeOut_s = 0.016;
 
 phaseIter = 3;
 
-% Convert from seconds to samples, make sure it's odd
-nF = 1 + 2*round(window_s * sr / 2);
+% Convert from seconds to samples, make sure it's even
+nFft = 2*round(window_s * sr / 2);
 dur  = round(dur_s * sr);
-nFft = (nF-1)*2;
 hop  = round(hopFrac * nFft);
 
 profile = speechProfile(sr, nFft, hop, noiseShape);
@@ -26,10 +26,14 @@ noiseSpec = stft(whiteNoise, nFft, nFft, hop);
 if isempty(mask), mask = ones(size(noiseSpec)); end
 
 mask = bsxfun(@times, profile, mask);
+mask = cat(2, mask, repmat(mask(:,end), 1, size(noiseSpec,2) - size(mask,2)));
 noiseSpec = mask .* noiseSpec;
 if nargout > 2
     [noise noiseSpec] = phaseRecon(noiseSpec, noiseSpec, phaseIter, nFft, hop);
     noise = noise(1:dur)';
+    
+    fadeOut = round(fadeOut_s * sr);
+    noise(end-fadeOut:end) = ((fadeOut:-1:0)/fadeOut)' .* noise(end-fadeOut:end);
 end
 
 doPlot = 0;
