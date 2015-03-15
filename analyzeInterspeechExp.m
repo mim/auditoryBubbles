2f14dd71a2629a2a0a437b1692af63375e333669
Michael Mandel <mim@mr-pc.org>
2015-03-15 08:08:35 -0400

    analyze original listening tests using latest code

---
+++ analyzeInterspeechExp.m
@@ -0,0 +1,21 @@
+function analyzeInterspeechExp()
+
+mixDir = 'D:\mixes\shannon\combined\bps15';
+[~,inCsvFiles] = findFiles('D:\Box Sync\data\mrt\shannonResults\orig', '.*.csv');
+
+resultFile = 'D:\Box Sync\data\mrt\shannonResults\orig\resultsNew.mat';
+verbose = 1;
+ignoreStimulusDir = 1;
+processListeningData(inCsvFiles, resultFile, verbose, ignoreStimulusDir);
+
+baseFeatDir = 'C:\Temp\data\origExp';
+pattern = 'bps15.*.wav';
+noiseShape = 0;        % whatever you use for your noise shape
+pcaDims = [100 1000];  % 100 dimensions from 1000 files
+usePcaDims = 70;
+trimFrames = 30;
+overwrite = 0;
+hop_s = 0.016;         % this is the default hop size used in the analysis
+setLength_s = 2.2;
+maxFreq_hz = 8000;
+mainBubbleAnalysis(mixDir, resultFile, baseFeatDir, pattern, noiseShape, pcaDims, usePcaDims, trimFrames, hop_s, overwrite, setLength_s, maxFreq_hz);
