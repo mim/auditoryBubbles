331e6c56baa7993c74f1060d73ffbe85dc660a24
Michael Mandel <mim@mr-pc.org>
2016-08-19 10:20:55 -0400

    added simulations of histograms, fixed some paths

--- analyzePreExps.m
+++ analyzePreExps.m
@@ -1,10 +1,11 @@
-function analyzePreExps()
+function analyzePreExps(toDisk, startAt)
 
 % Analyze data from pre experiments 1 and 2
 
-analysisDir = '/home/mim/work/papers/jasa14/pics4/agreement';
-toDisk = 0;
-startAt = 0;
+if ~exist('toDisk', 'var') || isempty(toDisk), toDisk = 0; end
+if ~exist('startAt', 'var') || isempty(startAt), startAt = 0; end
+
+analysisDir = '/home/mim/work/papers/jasa14/pics5/agreement';
 prt('ToFile', toDisk, 'StartAt', startAt, ...
     'Width', 3.5, 'Height', 3.5, 'NumberPlots', 0, ...
     'TargetDir', analysisDir, ...
@@ -21,16 +22,33 @@ for i=1:size(grouped,1),
     grouped{i,7} = [grouped{i,1}{1} '_' regexprep(grouped{i,3}, '_\d+.wav', '')]; 
 end
 grouped2 = groupBy(grouped, 7, @(x) mean([x{:}]), 5);
-labelHist([grouped2{:,5}], 0.2:.1:1, 'withinUserConsistency');
+%labelHist([grouped2{:,5}], 0.2:.1:1, 'withinUserConsistency');
+labelHist([grouped2{:,5}], 0:.1:1, 'withinUserConsistency');
+
+% Simulate same data with chance performance
+sim1 = mean(rand(10,3500) < (1/6), 1);
+sim2 = mean(rand(10,6500) < 0.95, 1);
+labelHist([sim1 sim2], 0:.1:1, 'simulatedWithinUserPerformance')
+
+% Simulate same data with chance performance
+sim1 = mean(rand(10,3500) < (1/6), 1);
+sim2 = nan * ones(1,6500);
+labelHist([sim1 sim2], 0:.1:1, 'simulatedWithinUserGuessing', 0.35, 0.4)
+
 
 % Inter-subject agreement of accuracies of repeated listenings
 nSubj = length(files);
 for j=1:nSubj
     subjAcc(:,j) = [grouped2{j:nSubj:end,5}]';
 end
-[~,subAxes] = plotmatrix(lim(subjAcc - 0.05*rand(size(subjAcc)), 0, 1), 'o');
+[~,subAxes,~,~,histAxes] = plotmatrix(lim(subjAcc - 0.05*rand(size(subjAcc)), 0, 1), 'o');
 for i=1:size(subAxes,2)
     title(subAxes(1,i), sprintf('Subj %d', i), 'fontweight', 'normal'); 
+end
+hyl = get(histAxes, 'YLim');
+newHYL = max(cat(1, hyl{:}), [], 1);
+for i=1:length(histAxes)
+    set(histAxes, 'YLim', newHYL);
 end
 for i=1:size(subAxes,1)
     set(subAxes(i,end), 'YAxisLocation', 'right')
@@ -46,13 +64,20 @@ verbose = 1;
 [~,inCsvFiles] = findFiles('/home/data/bubblesResults/shannonResults/preExps/', 'pre2.*.csv');
 %processListeningData(inCsvFiles, resultFile, verbose);
 load(resultFile)
-labelHist([grouped{:,5}], 0.2:0.2:1, 'acrossUserConsistency');
+%labelHist([grouped{:,5}], 0.2:0.2:1, 'acrossUserConsistency');
+labelHist([grouped{:,5}], 0:0.2:1, 'acrossUserConsistency');
+
+% Simulate same data with chance performance
+sim1 = mean(rand(5,3500) < (1/6), 1);
+sim2 = mean(rand(5,6500) < 0.9, 1);
+labelHist([sim1 sim2], 0:.2:1, 'simulatedAcrossUserPerformance')
 
 return
 
 
 % Do bubbles processing on combined pre2 data
-mixDir = 'D:\mixes\shannon\oneSpeaker15bps';
+%mixDir = '/mnt/bighd/backup/osu/D/mixes/shannon/oneSpeaker15bps';
+mixDir = '/home/data/bubbles/shannon/combined/bps15';
 pattern = 'bps15.*.wav';
 noiseShape = 0;
 pcaDims = [100 1000];  % 100 dimensions from 1000 files
@@ -65,7 +90,7 @@ mainBubbleAnalysis(mixDir, resultFile, analysisDir, pattern, noiseShape, pcaDims
 
 % Do bubbles processing on each pre2 data separately
 for i = 1:length(inCsvFiles)
-    resultFile = fullfile('D:\Box Sync\data\mrt\shannonResults\preExps', sprintf('grouped_pre2sub%d.mat', i));
+    resultFile = fullfile('/home/data/bubblesResults/shannonResults/preExps', sprintf('grouped_pre2sub%d.mat', i));
     processListeningData(inCsvFiles{i}, resultFile, verbose);
     load(resultFile);
     listenerCorrect(:,i) = [grouped{:,5}]';
@@ -94,12 +119,16 @@ kr
 % TODO: do this
 
 
-function labelHist(vals, bins, fileName)
+function labelHist(vals, bins, fileName, total, ymax)
+
+if ~exist('total', 'var') || isempty(total), total = 1; end
+if ~exist('ymax', 'var'), ymax = []; end
 
 [h x] = hist(vals, bins);
-bar(x, h / sum(h), 'hist')
+bar(x, total * h / sum(h), 'hist')
 halfBarWidth = mean(diff(x)) / 2;
 xlim([min(x)-halfBarWidth max(x)+halfBarWidth]);
+if ~isempty(ymax), ylim([0 ymax]), end
 ylabel('Proportion of total')
 xlabel('Proportion correct')
 %title('Proportion correct of each mixture of 10 repeated listenings by the same subject')
