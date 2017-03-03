c8423dc7eadfee4d930305f5b39a91ce141085e3
Michael Mandel <mim@mr-pc.org>
2017-03-02 21:21:47 -0500

    use max approximation in creating bubble masks to greatly speed up computations for long noise samples

--- genMaskFromBubbleLocs.m
+++ genMaskFromBubbleLocs.m
@@ -1,19 +1,33 @@
 function mask = genMaskFromBubbleLocs(nF, nT, freqVec_erb, ...
-    timeVec_s, bubbleF_erb, bubbleT_s, sizeT_s, sizeF_erb, makeHoles, suppressHolesTo_db)
+    timeVec_s, bubbleF_erb, bubbleT_s, sizeT_s, sizeF_erb, makeHoles, suppressHolesTo_db, maxApprox)
 
 if ~exist('makeHoles', 'var') || isempty(makeHoles), makeHoles = false; end
 if ~exist('sizeF_erb', 'var') || isempty(sizeF_erb), sizeF_erb = 0.4; end
 if ~exist('sizeT_s', 'var') || isempty(sizeT_s), sizeT_s  = 0.02; end
 if ~exist('suppressHolesTo_db', 'var') || isempty(suppressHolesTo_db), suppressHolesTo_db = -80; end
+if ~exist('maxApprox', 'var') || isempty(maxApprox), maxApprox = 1; end
 
 if all(isfinite(bubbleF_erb))
     [times_s freqs_erb] = meshgrid(timeVec_s, freqVec_erb);
-    mask = zeros(nF, nT);
+    if maxApprox
+        mask = -200 * ones(nF, nT);
+    else
+        mask = zeros(nF, nT);
+    end
+    
     for i = 1:length(bubbleF_erb)
         bumpDb = -(times_s - bubbleT_s(i)).^2 / sizeT_s.^2 ...
-                 - (freqs_erb - bubbleF_erb(i)).^2 / sizeF_erb.^2;
+            - (freqs_erb - bubbleF_erb(i)).^2 / sizeF_erb.^2;
         
-        mask = mask + 10.^(bumpDb / 20);
+        if maxApprox
+            mask = max(mask, bumpDb);
+        else
+            bumpLin = 10.^(bumpDb / 20);
+            mask = mask + bumpLin;
+        end
+    end
+    if maxApprox
+        mask = 10.^(mask / 20);
     end
 else
     % Infinite bubbles
